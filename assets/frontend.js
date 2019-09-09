e703529a12bc8a3eeaad9f1d53d38f84d4e80c28
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-09-09 16:26:32 +0200

    event listeners

--- assets/frontend.js
+++ assets/frontend.js
@@ -5,3 +5,17 @@ const addPlaylist = document.querySelector('.fa-plus');
 const addToFavorites = document.querySelector('.favorite');
 const tracks = document.querySelector('ol');
 const playlists = document.querySelector('.playlist');
+const audio = document.querySelector('audio');
+
+audio.addEventListener('loadstart', e => {
+    console.log(`${e.type} happened`);
+});
+audio.addEventListener('play', e => {
+    console.log(`${e.type} happened`);
+});
+audio.addEventListener('ended', e => {
+    console.log(`${e.type} happened`);
+});
+audio.addEventListener('progress', e => {
+    console.log(`${e.type} happened`);
+});
