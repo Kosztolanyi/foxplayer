d36ac27c382ca254dd29233f3c14df3a07584739
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-09-11 12:12:51 +0200

    delete from playlist

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,35 +1,38 @@
-'use strict';
+"use strict";
 
-const addToPlayListButton = document.querySelector('.add');
-const addPlaylist = document.querySelector('.playlist-header > button');
-const addToFavorites = document.querySelector('.favorite');
-const tracks = document.querySelector('ol');
-const playlists = document.querySelector('.playlist');
-const audio = document.querySelector('audio');
-
-function createDivToPlaylist(innerText,id) {
-  let div = document.createElement('div');
+const addToPlayListButton = document.querySelector(".add");
+const addPlaylist = document.querySelector(".playlist-header > button");
+const addToFavorites = document.querySelector(".favorite");
+const tracks = document.querySelector("ol");
+const playlists = document.querySelector(".playlist");
+const audio = document.querySelector("audio");
+const deletebuttons = document.q
+//FUNCTIONS
+function createDivToPlaylist(innerText, id) {
+  let div = document.createElement("div");
   div.innerHTML = innerText;
-  div.id = id;
-  div.className = 'tracks-in-list';
-  let button = document.createElement('button');
+  div.classList = `'${id} tracks-in-list`;
+  let button = document.createElement("button");
+  button.className = id;
   button.innerHTML = '<i class="far fa-trash-alt"></i>';
   div.appendChild(button);
   return div;
 }
-window.addEventListener('load', e => {
-  fetch('/tracks')
+//LOAD
+window.addEventListener("load", e => {
+  fetch("/tracks")
     .then(res => res.json())
     .then(json => {
-      let h2 = document.querySelector('.details > h2');
+      let h2 = document.querySelector(".details > h2");
       h2.innerText = json[0].title;
-      let p = document.querySelector('.details > p');
+      let p = document.querySelector(".details > p");
       p.innerText = json[0].artist;
       json.forEach(e => {
-        let li = document.createElement('li');
-        let span1 = document.createElement('span');
-        let span2 = document.createElement('span');
-        span2.className = 'time';
+        let li = document.createElement("li");
+        li.className = e.id;
+        let span1 = document.createElement("span");
+        let span2 = document.createElement("span");
+        span2.className = "time";
         span1.innerText = e.title;
         span2.innerText = `${Math.floor(e.duration / 60)}:${Math.floor(
           e.duration
@@ -41,55 +44,59 @@ window.addEventListener('load', e => {
       });
     });
 });
-window.addEventListener('load', e => {
-  fetch('/playlist')
+window.addEventListener("load", e => {
+  fetch("/playlist")
     .then(res => res.json())
     .then(json => {
       json.forEach(e => {
-        if (e.playlist !== '0') {
-          playlists.appendChild(createDivToPlaylist(e.playlist,e.id));
+        if (e.playlist !== "0") {
+          playlists.appendChild(createDivToPlaylist(e.playlist, e.id));
         }
       });
     })
     .catch(console.log);
 });
-audio.addEventListener('loadstart', e => {
-  console.log(`${e.type} happened`);
-});
-audio.addEventListener('play', e => {
-  console.log(`${e.type} happened`);
-});
-audio.addEventListener('ended', e => {
-  console.log(`${e.type} happened`);
-});
-audio.addEventListener('progress', e => {
-  console.log(`${e.type} happened`);
-});
 
-addPlaylist.addEventListener('click', e => {
-  if (document.querySelector('input')) {
+//AUDIO EVENTLISTENERS
+audio.addEventListener("loadstart", e => {
+  console.log(`${e.type} happened`);
+});
+audio.addEventListener("play", e => {
+  console.log(`${e.type} happened`);
+});
+audio.addEventListener("ended", e => {
+  console.log(`${e.type} happened`);
+});
+audio.addEventListener("progress", e => {
+  console.log(`${e.type} happened`);
+});
+//CLICKS
+addPlaylist.addEventListener("click", e => {
+  if (document.querySelector("input")) {
     return;
   } else {
     e.preventDefault();
-    alert('Type & hit enter!');
-    let newPlaylist = document.createElement('input');
-    newPlaylist.setAttribute('type', 'text');
+    alert("Type & hit enter!");
+    //creating input
+    let newPlaylist = document.createElement("input");
+    newPlaylist.setAttribute("type", "text");
     playlists.appendChild(newPlaylist);
     newPlaylist.focus();
-    let input = document.querySelector('input');
-    input.addEventListener('keypress', e => {
-      if (e.key === 'Enter') {
-        fetch('/playlists', {
-          method: 'POST',
+    //POST to database
+    let input = document.querySelector("input");
+    input.addEventListener("keypress", e => {
+      if (e.key === "Enter") {
+        fetch("/playlists", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
           },
           body: JSON.stringify({ playlist: input.value })
         })
           .then(res => res.json())
           .then(json => {
             console.log(json);
-            let newDiv = createDivToPlaylist(input.value,json.insertId);
+            let newDiv = createDivToPlaylist(input.value, json.insertId);
             playlists.removeChild(playlists.lastChild);
             playlists.appendChild(newDiv);
           });
@@ -98,7 +105,18 @@ addPlaylist.addEventListener('click', e => {
   }
 });
 
-document.addEventListener('keypress', e => {
+playlists.addEventListener('click', e => {
+  if(e.target.className = 'fa-trash-alt') {
+    e.preventDefault();
+    const audioId = `${e.target.parentElement.className}`;
+    console.log(audioId);
+      fetch(`/playlists/${audioId}`, {
+        method: 'DELETE',
+      }).then(res => res.json()).then(json =>console.log(json, "ok")).catch(console.log)
+  }
+})
+//KEYPRESS
+document.addEventListener("keypress", e => {
   switch (e.charCode) {
     case 32:
       if (!audio.paused) {
@@ -109,8 +127,8 @@ document.addEventListener('keypress', e => {
       break;
   }
 });
-document.addEventListener('keyup', e => {
-  if (e.key === 'Escape') {
+document.addEventListener("keyup", e => {
+  if (e.key === "Escape") {
     if (audio.muted) {
       audio.muted = false;
     } else {
--- routes.js
+++ routes.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const fs = require('fs');
-const mm = require('musicmetadata');
+const mysql = require("mysql");
+const fs = require("fs");
+const mm = require("musicmetadata");
 //Write your params here
-const databaseName = 'foxplayer';
-const databasePassword = 'password1234';
+const databaseName = "foxplayer";
+const databasePassword = "password1234";
 app.use(express.json());
-app.use(express.static('assets'));
-app.use(express.static('views'));
-
-function collectMetaData(e) {
+app.use(express.static("assets"));
+app.use(express.static("views"));
+// mm(fs.createReadStream("./assets/audio/01-rage_against_the_machine-people_of_the_sun-ksi_int.mp3"), (err,metadata) => {
+//   console.log(metadata.albumartist);
+// })
+function collectMetaData(row) {
   return new Promise((resolve, reject) => {
-    mm(fs.createReadStream(e.path), { duration: true }, (err, metadata) => {
+    mm(fs.createReadStream(row.path), { duration: true }, (err, metadata) => {
       if (err) {
         reject(err);
       } else {
@@ -21,61 +23,70 @@ function collectMetaData(e) {
           artist: metadata.artist[0],
           album: metadata.album,
           duration: metadata.duration,
-          path: e.path
+          path: row.path
         });
       }
     });
   });
 }
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: "localhost",
+  user: "root",
   password: databasePassword,
   database: databaseName
 });
 
 connection.connect(err => {
   if (err) {
-    console.log('error connecting with the database');
+    console.log("error connecting with the database");
     console.log(err.message);
     return;
   }
-  console.log('database connection is established');
+  console.log("database connection is established");
 });
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.get('/playlist', (req, res) => {
-  connection.query('select * from playlists;', async (err, rows) => {
+app.get("/playlist", (req, res) => {
+  connection.query("select * from playlists;", async (err, rows) => {
     if (err) {
       console.log(err.message);
     }
     res.send(rows);
   });
 });
-app.post('/playlists', (req,res) => {
+app.post("/playlists", (req, res) => {
   const newPlaylist = req.body.playlist;
-  connection.query('INSERT INTO playlists(playlist) value(?);', newPlaylist, (err, rows) => {
-    if (err) {
-      console.log(err.message);
-      res.status(401);
-      return;
+  connection.query(
+    "INSERT INTO playlists(playlist) value(?);",
+    newPlaylist,
+    (err, rows) => {
+      if (err) {
+        console.log(err.message);
+        res.status(401);
+        return;
+      }
+      res.status(200);
+      res.send(rows);
     }
-    res.status(200);
-    res.send(rows);
-  });
-  // res.sendStatus(200);
-})
-//sql query
-app.get('/tracks', (req, res) => {
-  connection.query('select * from tracks', async (err, rows) => {
+  );
+});
+app.get("/tracks", (req, res) => {
+  connection.query("select * from tracks", async (err, rows) => {
     if (err) {
       console.log(err.message);
     }
     res.send(await Promise.all(rows.map(collectMetaData)));
   });
 });
-
+app.delete('/playlists/:id', (req, res) => {
+  connection.query("DELETE FROM playlists WHERE id=?;", [req.params.id] ,(err, rows) => {
+    if (err) {
+      console.log(err.message, "wrong");
+    }
+    res.send(rows);
+  });
+})
 module.exports = app;
